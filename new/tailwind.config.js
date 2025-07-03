72f0cd6eab4641eda3593efa23b8bea5f745bae1
Abidmuhammad999 <64603172+Abidmuhammad999@users.noreply.github.com>
2025-07-03 14:47:33 +0530

    color change

---
+++ new/tailwind.config.js
@@ -0,0 +1,23 @@
+// myconfig.js
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        forestTeal: {
+          900: '#1d3b33',
+          800: '#264f44',
+          700: '#306458',
+          600: '#3d7d6b',
+          500: '#4e9983',
+          400: '#63bba0',
+          300: '#84d5b9',
+          200: '#a8e9cf',
+          100: '#d3f8ea',
+        }
+      },
+      fontFamily: {
+        opensans: ['"Open Sans"', 'sans-serif'],
+      }
+    }
+  }
+}
